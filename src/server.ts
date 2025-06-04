22549658b06e3c202bae07878fcee35ed52b9ec8
Eduardo Ferreira Leite <eduardoferreiraleite.dev@gmail.com>
2025-06-04 01:47:10 -0300

    SupaBaseIntegration

--- src/server.ts
+++ src/server.ts
@@ -26,12 +26,23 @@ app.post("/users", async (req, res) => {
     try {
         const { name, email } = req.body as Users
 
-        const{ data: createdUser } = await supabase.from("users").insert([
+        const { data: createdUser } = await supabase.from("users").insert([{
             name,
             email
-        ])
+        }]).select()
+
+        return{
+            value: createdUser ? createdUser [0] : null //verificação pra ver se tem o createUser ele ira devolver na posição 0 e se nn ele vai devolver nulo
+        }
     } catch (error) {
         console.error(error);
         throw error;
     }
+})
+
+app.listen({
+    host: '0.0.0.0',
+    port:  process.env.PORT ? Number(process.env.PORT) : 3002 //Se houver process.env.PORT ira devolver ele msm (?), se nn era devolver a porta 3002(:)
+}).then(() =>{
+    console.log('Servidor funcionando!')
 })
